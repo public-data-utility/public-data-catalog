b9446e65127532788f20756945d3ff5f7e1f9443
Mark davis <github@nohorse.com>
2022-09-10 13:37:38 -0700

    updated ooqtane references

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,7 +1,7 @@
-XCOPY "..\Client\bin\Debug\net6.0\Pdu.Catalog.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Client\bin\Debug\net6.0\Pdu.Catalog.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Server\bin\Debug\net6.0\Pdu.Catalog.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Server\bin\Debug\net6.0\Pdu.Catalog.Server.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Shared\bin\Debug\net6.0\Pdu.Catalog.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
-XCOPY "..\Shared\bin\Debug\net6.0\Pdu.Catalog.Shared.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Client\bin\Debug\net6.0\Pdu.Catalog.Client.Oqtane.dll" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Client\bin\Debug\net6.0\Pdu.Catalog.Client.Oqtane.pdb" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Server\bin\Debug\net6.0\Pdu.Catalog.Server.Oqtane.dll" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Server\bin\Debug\net6.0\Pdu.Catalog.Server.Oqtane.pdb" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Shared\bin\Debug\net6.0\Pdu.Catalog.Shared.Oqtane.dll" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
+XCOPY "..\Shared\bin\Debug\net6.0\Pdu.Catalog.Shared.Oqtane.pdb" "..\oqtane.framework\Oqtane.Server\bin\Debug\net6.0\" /Y
 XCOPY "..\Server\wwwroot\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\" /Y /S /I
